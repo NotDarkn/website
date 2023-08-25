2a4683de728435afa2f607cc9f675c0c1b4c73f0
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-08-24 21:48:06 -0400

    added e-halycon styled builder, not real though

---
+++ public/sh1mmer/fog.js
@@ -0,0 +1,4 @@
+//without this the website will not run
+window.onload = function () {
+  console.log('%c E-HALYCON!!', 'font-weight: bold; font-size: 50px;color: lightgray; text-shadow: 3px 3px 0 rgb(144, 140, 139) , 6px 6px 0 rgb(95, 92, 91) , 9px 9px 0 rgb(65, 61, 61) , 12px 12px 0 rgb(43, 40, 39) , 15px 15px 0 rgb(26, 23, 23) , 18px 18px 0 rgb(15, 14, 13) , 21px 21px 0 rgb(0, 0, 0');
+};
