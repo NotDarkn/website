c2227b08fb601fc2e1c8efd1450aea17c443933a
wave <73656336+13waves@users.noreply.github.com>
2023-10-24 00:15:22 +0300

    fix: disabled SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://osu.bio",
-  output: "server",
-  adapter: cloudflare()
+  site: "https://osu.bio"
 });
