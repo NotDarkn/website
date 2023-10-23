c1cd13be592e200b66163256b6ad43135fe3b66f
wave <73656336+13waves@users.noreply.github.com>
2023-10-24 01:58:26 +0300

    small changes and converts (#18)
    * chore; update astro and make fonts better
    
    * fix: fixed fonts lol
    
    * fix: disabled SSR
    
    * chore: update pnpm-lock.yaml to reflect changes
    
    * chore: readded SSR as it isnt causing any issues
    
    * feat: renamed sh1mmer html files to astro
    
    * fix: added is:inline to a file
    
    * fix: fixed button spacing
    
    * fix: fixed button issues
    
    * fix: should fix css font issues, still in testing
    
    * fix: reverted font stuff since font is too big

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://osu.bio",
+  output: "server",
+  adapter: cloudflare()
 });
