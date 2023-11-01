569ad9037bf45b31e1e350316bd1fe8a70e98804
13waves <73656336+13waves@users.noreply.github.com>
2023-11-01 18:25:29 +0200

    feat: added better alerts and quirks to downloader

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://osu.bio",
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [react()]
 });
