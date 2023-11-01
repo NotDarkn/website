70e185b353b3970e800cf9c6f5511160444cc3de
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-01 17:00:21 -0400

    lol added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
+
 import cloudflare from "@astrojs/cloudflare";
 
 import react from "@astrojs/react";
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://osu.bio",
   output: "server",
   adapter: cloudflare(),
-  integrations: [react()]
+  integrations: [react(), partytown()]
 });
