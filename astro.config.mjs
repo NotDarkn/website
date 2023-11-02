da8a5f3d24c8f5cf0665989d4f142774864e24e4
wave <73656336+13waves@users.noreply.github.com>
2023-11-02 13:29:16 +0200

    improve QOL changes and minor fixes (#20)
    * fix: improve QOL changes and minor fixes
    
    This commit includes several small QOL improvements and bug fixes to enhance user interaction and overall performance.
    
    * Update selection functionality in downloader.astro
    
    Improved UI interaction in src/pages/sh1mmer/downloader.astro where 'boardd' and 'downloadd' hide when no 'mirror' is selected. This is to prevent user confusion or error.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
-
 import partytown from '@astrojs/partytown';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://osu.bio",
   output: "server",
   adapter: cloudflare(),
-  integrations: [react(), partytown()]
+  integrations: [react(), partytown(), sitemap()]
 });
