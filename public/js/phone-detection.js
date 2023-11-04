3876768bd6ed76c46e817897083df2906e207a7e
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-03 20:54:25 -0400

    change: number of buttons in alt, remove detector

--- public/js/phone-detection.js
+++
@@ -1,9 +0,0 @@
-function isMobile() {
-    return /iPhone|iPad|iPod|Android|Windows Phone|webOS|BlackBerry|IEMobile|Opera Mini|Mobile Safari|Mobile Chrome|Kindle|Silk|BB10|PlayBook|SymbianOS|Maemo|Meego|Bada|Windows Mobile|UP.Browser|UCWEB|Skyfire|Dolfin|Fennec|Mobile|Tablet|Touch|WAP|Mini/i.test(navigator.userAgent);
-}
-
-var imageElement = document.getElementById("sakamatoImage");
-
-if (isMobile()) {
-    imageElement.src = "img/sakamato-alt.PNG";
-}
