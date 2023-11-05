725558a08b42c44e7274d66542dd6f84e6685e9e
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-04 23:59:04 -0400

    fix: snowflakes javascript code -ChatGPT

--- public/js/snowflakes.js
+++ public/js/snowflakes.js
@@ -1,14 +1,16 @@
-// Number of snowflakes you want
-const numSnowflakes = 50;
-
-// Get the container element where the snowflakes will be added
-const container = document.getElementById('snowflake-container');
-
-for (let i = 0; i < numSnowflakes; i++) {
-  // Create a new div element
-  const snowflake = document.createElement('div');
-  snowflake.className = 'snowflake'; // Add the "snowflake" class
-
-  // Append the snowflake to the container
-  container.appendChild(snowflake);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    // Number of snowflakes you want
+    const numSnowflakes = 50;
+  
+    // Get the container element where the snowflakes will be added
+    const container = document.getElementById('snowflakes-container');
+  
+    for (let i = 0; i < numSnowflakes; i++) {
+      // Create a new div element
+      const snowflake = document.createElement('div');
+      snowflake.className = 'snowflake'; // Add the "snowflake" class
+  
+      // Append the snowflake to the container
+      container.appendChild(snowflake);
+    }
+  });
