5f1deca50c38609809eb4b2136334e8d92b1d6cf
wave <wave301@icloud.com>
2023-11-05 16:48:53 +0200

    fix(snowflakes): make them actually work

--- public/js/snowflakes.js
+++ public/js/snowflakes.js
@@ -1,20 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Number of snowflakes you want
+function createSnowflakes() {
     const numSnowflakes = 50;
-  
-    // Get the container element where the snowflakes will be added
+    const maxSize = 0.2;
+    const maxTranslate = 10;
     const container = document.getElementById('snowflake-container');
-  
-    if (container) {
-      for (let i = 0; i < numSnowflakes; i++) {
-        // Create a new div element
-        const snowflake = document.createElement('div');
-        snowflake.className = 'snowflake'; // Add the "snowflake" class
-  
-        // Append the snowflake to the container
-        container.appendChild(snowflake);
-      }
-    } else {
-      console.error('The snowflake-container element was not found in the HTML.');
+
+    // Function to generate a random number from min to max
+    function randomBetween(min, max) {
+        return (Math.random() * max) + min;
     }
-  });
+
+    if (container) {
+        for (let i = 0; i < numSnowflakes; i++) {
+            let randomSize = randomBetween(5, 25) + "px";
+            let randomLeft = randomBetween(0, window.innerWidth) + "px";
+            let randomDuration = randomBetween(3, 10);
+
+            // Create div and apply styles
+            const snowflake = document.createElement('div');
+            snowflake.classList.add('snowflake');
+            snowflake.style.width = randomSize;
+            snowflake.style.height = randomSize;
+            snowflake.style.left = randomLeft;
+            snowflake.style.animationDuration = randomDuration + "s";
+            snowflake.style.animationDelay = randomBetween(0, 5) + "s";
+
+            container.appendChild(snowflake);
+        }
+    } else {
+        console.error('The snowflake-container element was not found in the HTML.');
+    }
+}
+
+createSnowflakes();
