13c042831498d957b3a31667e23a731517e06fca
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-06 07:45:06 -0500

    attempt 23: organize iOS header fix

--- public/js/mobiledetection.js
+++ public/js/mobiledetection.js
@@ -1,12 +1,10 @@
-// phind.com code
-window.addEventListener('resize', function() {
-    let isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone|Mobi/i.test(navigator.userAgent);
-    let header = document.querySelector('.header-height');
-    if (isMobileDevice) {
-        header.classList.remove('header-height');
-        header.classList.add('mobile-header-height');
-    } else {
-        header.classList.remove('mobile-header-height');
-        header.classList.add('header-height');
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+};
+
+window.onload = function() {
+    if(isMobileDevice()) {
+        var element = document.querySelector(".header-height");
+        element.className = "mobile-header-height";
     }
- });    
+};
