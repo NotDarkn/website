a9e6b1b5623ce4f25d97289ea6feaf6c10481d0f
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-08 08:29:07 -0500

    update: sh1mmer files

--- public/sh1mmer/files/script.js
+++
@@ -1,7 +0,0 @@
-// the site can run w/o this just MW likes this file for some reason lol
-window.onload = function () {
-  console.log(
-    "%c SH1MMER!!",
-    "font-weight: bold; font-size: 50px;color: purple; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)",
-  );
-};
