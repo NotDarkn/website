1b1bf9ccdd3c3e5a120eadc030edffd26038ee5d
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-12 02:50:21 -0500

    attempt(2): edit some things

---
+++ public/js/removeclass.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    window.setTimeout(function() {
+      document.querySelector('.fade-in').classList.remove('fade-in');
+    }, 15000); 
+});
