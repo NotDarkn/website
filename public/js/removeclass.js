620410f398e4421b20548d87f898309f00704433
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-12 02:57:14 -0500

    attempt(3): customize fadeins

--- public/js/removeclass.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    window.setTimeout(function() {
-      document.querySelector('.fade-in').classList.remove('fade-in');
-    }, 15000); 
-});
