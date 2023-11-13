f59f88dc7b9eefc58b51ef2bf9cc164a2e41d31b
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-11-12 20:06:50 -0500

    update: script calls and icon speed

---
+++ public/js/mobile404.ts
@@ -0,0 +1,10 @@
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Samsung|LG|Sony|Nokia|HTC|Windows Phone|Kindle|PlayBook|BB10|BlackBerry|ZuneWP7|ZuneWP7.5|ZuneWP8|ZuneWP8.5|ZuneWP10|Silk/i.test(navigator.userAgent);
+};
+
+window.onload = function() {
+    if(isMobileDevice()) {
+        var element = document.querySelector(".header-height-404");
+        element.className = "mobile-header-height-404";
+    }
+};
