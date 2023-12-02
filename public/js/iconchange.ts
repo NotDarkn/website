692cac3b9f0d3d2875b3a648e62be2ce671ab2cf
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-12-02 18:25:18 -0500

    add: icon opacity hover after finish

--- public/js/iconchange.ts
+++ public/js/iconchange.ts
@@ -1,10 +1,12 @@
-var elements = document.querySelectorAll('.icons-a, .icons-b, .icons-c, .icons-d, .icons-e');
+var classNames = ['icons-a', 'icons-b', 'icons-c', 'icons-d', 'icons-e'];
+var delays = [3975, 4200, 4425, 4650, 4875];
 
-function changeClasses() {
-   for (var i = 0; i < elements.length; i++) {
-       elements[i].classList.remove('icons-a', 'icons-b', 'icons-c', 'icons-d', 'icons-e');
-       elements[i].classList.add('icon-js');
-   }
-}
-
-setTimeout(changeClasses, 4650);
+classNames.forEach((className, index) => {
+   setTimeout(() => {
+       var elements = document.querySelectorAll('.' + className);
+       for (var i = 0; i < elements.length; i++) {
+           elements[i].classList.remove(className);
+           elements[i].classList.add('icon-js');
+       }
+   }, delays[index]);
+});
