04d21b7ef2f6e8f206ed644ae4e4d4358994e3b9
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-12-02 18:29:10 -0500

    fix: broken timing
    this is with AI

--- public/js/iconchange.ts
+++ public/js/iconchange.ts
@@ -1,12 +1,46 @@
-var classNames = ['icons-a', 'icons-b', 'icons-c', 'icons-d', 'icons-e'];
-var delays = [3975, 4200, 4425, 4650, 4875];
-
-classNames.forEach((className, index) => {
-   setTimeout(() => {
-       var elements = document.querySelectorAll('.' + className);
-       for (var i = 0; i < elements.length; i++) {
-           elements[i].classList.remove(className);
-           elements[i].classList.add('icon-js');
-       }
-   }, delays[index]);
-});
+function changeClassA() {
+    var elements = document.querySelectorAll('.icons-a');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('icons-a');
+        elements[i].classList.add('icon-js');
+    }
+   }
+   
+   function changeClassB() {
+    var elements = document.querySelectorAll('.icons-b');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('icons-b');
+        elements[i].classList.add('icon-js');
+    }
+   }
+   
+   function changeClassC() {
+    var elements = document.querySelectorAll('.icons-c');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('icons-c');
+        elements[i].classList.add('icon-js');
+    }
+   }
+   
+   function changeClassD() {
+    var elements = document.querySelectorAll('.icons-d');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('icons-d');
+        elements[i].classList.add('icon-js');
+    }
+   }
+   
+   function changeClassE() {
+    var elements = document.querySelectorAll('.icons-e');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('icons-e');
+        elements[i].classList.add('icon-js');
+    }
+   }
+   
+   setTimeout(changeClassA, 3975);
+   setTimeout(changeClassB, 4200);
+   setTimeout(changeClassC, 4425);
+   setTimeout(changeClassD, 4650);
+   setTimeout(changeClassE, 4875);
+   
