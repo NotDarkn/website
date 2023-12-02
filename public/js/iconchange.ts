32a36f876f425a36f55be278578b2b8680b82420
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-12-02 18:37:19 -0500

    change: revert to old script

--- public/js/iconchange.ts
+++ public/js/iconchange.ts
@@ -1,12 +1,10 @@
-var classNames = ['icons-a', 'icons-b', 'icons-c', 'icons-d', 'icons-e'];
-var delays = [3775, 4000, 4225, 4450, 4675];
+var elements = document.querySelectorAll('.icons-a, .icons-b, .icons-c, .icons-d, .icons-e');
 
-classNames.forEach((className, index) => {
-   setTimeout(() => {
-       var elements = document.querySelectorAll('.' + className);
-       for (var i = 0; i < elements.length; i++) {
-           elements[i].classList.remove(className);
-           elements[i].classList.add('icon-js');
-       }
-   }, delays[index]);
-});
+function changeClasses() {
+   for (var i = 0; i < elements.length; i++) {
+       elements[i].classList.remove('icons-a', 'icons-b', 'icons-c', 'icons-d', 'icons-e');
+       elements[i].classList.add('icon-js');
+   }
+}
+
+setTimeout(changeClasses, 4775);
