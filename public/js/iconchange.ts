8bdb4fa295ad4aa67de20c8e0a67e722df40371a
Darkn <73033672+NotDarkn@users.noreply.github.com>
2023-12-02 18:10:14 -0500

    add: class change for opacity hover
    will this work..?

---
+++ public/js/iconchange.ts
@@ -0,0 +1,7 @@
+var elements = document.querySelectorAll('.icons-a, .icons-b, .icons-c, .icons-d, .icons-e');
+
+setTimeout(() => {
+ elements.forEach(element => {
+   element.className = 'icon-js';
+ });
+}, 4650);
