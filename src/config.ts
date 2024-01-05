490b9889b6a1b59b34e9bb2715e4e643b768ed06
Darkn <73033672+NotDarkn@users.noreply.github.com>
2024-01-04 19:00:07 -0500

    change: delete home and about

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,9 @@
 import type { NavItems } from "./types";
 
 export const NAV_ITEMS: NavItems = {
-	home: {
-		path: "/",
-		title: "home",
-	},
 	blog: {
 		path: "/blog",
 		title: "blog",
-	},
-	about: {
-		path: "/about",
-		title: "about",
-	},
 };
 
 export const SITE = {
