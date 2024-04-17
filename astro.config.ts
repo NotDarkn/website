58f552e0f1676d95625ccd7d81f4a6c0d0e318a2
Darkn <privateservice000@gmail.com>
2024-04-17 10:21:16 -0400

    fix(config): old domain

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://osu.bio",
+  site: "https://darkn.bio",
   output: "server",
   adapter: process.env.CF_PAGES === '1' ? cloudflare() : vercel({
     webAnalytics: {
