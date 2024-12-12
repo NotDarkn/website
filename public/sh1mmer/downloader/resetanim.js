ca56b9c69b9ed873d2346da64e4c32234432fb81
wave <wave301@icloud.com>
2024-12-12 16:58:15 +0200

    chore: remove useless stuff and unused things

--- public/sh1mmer/downloader/resetanim.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('details').addEventListener('toggle', function() {
-  if (this.open) {
-      this.querySelectorAll('*:not(summary)').forEach(dropdown => {
-          dropdown.style.animation = 'none';
-          setTimeout(() => dropdown.style.animation = '', 10);
-      });
-  }
-});
